50278c46fd5a5a1e2aefc753a17c04fa8be063d9
Chris <comosjay@gmail.com>
2026-01-23 01:14:49 -0800

    gwad bless

--- deploy.php
+++
@@ -1,7 +0,0 @@
-<?php
-$repo_dir = '/var/www/chrisp.site';
-$output = shell_exec("cd $repo_dir && git pull origin main 2>&1");
-file_put_contents('deploy_log.txt', date('Y-m-d H:i:s') . "\n" . $output . "\n---\n", FILE_APPEND);
-
-echo "finished deploying";
-?>
