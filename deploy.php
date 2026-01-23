35f09c2bed30b7718e1bc83107a788c050fb45c7
Chris <comosjay@gmail.com>
2026-01-23 01:00:31 -0800

    sad

--- deploy.php
+++ deploy.php
@@ -1,6 +1,6 @@
 <?php
-$repo_dir = '/var/www/chrisp.site/CSE-135-HW-1';
-$output = shell_exec("cd $repo_dir && git pull origin master 2>&1");
+$repo_dir = '/var/www/chrisp.site';
+$output = shell_exec("cd $repo_dir && git pull origin main 2>&1");
 file_put_contents('deploy_log.txt', date('Y-m-d H:i:s') . "\n" . $output . "\n---\n", FILE_APPEND);
 
 echo "finished deploying";
