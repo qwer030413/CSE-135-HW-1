bde205bfe15b2708f815eaf3656b32cc389286d5
Chris <comosjay@gmail.com>
2026-01-22 21:34:22 -0800

    plss

--- deploy.php
+++ deploy.php
@@ -1,5 +1,5 @@
 <?php
-$output = shell_exec('cd /var/www/html && git pull 2>&1');
+$output = shell_exec('cd /var/www/chrisp.site/CSE-135-HW-1 && git pull 2>&1');
 
 file_put_contents('deploy_log.txt', date('Y-m-d H:i:s') . "\n" . $output . "\n---\n", FILE_APPEND);
 
